2270bf05f84040b76daa74f3f9331e03c3a4bfdd
leonidaspantoleon-coder <>
2026-03-18 11:58:28 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,9 @@
+# Assigning variables
+total_cost = 25.00
+discount_threshold = 20.00
+
+# Boolean variable
+discountEligible = total_cost >= discount_threshold
+
+# Output
+print("Is the purchase eligible for a discount?", discountEligible)
